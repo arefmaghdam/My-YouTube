1ce6552522041615a87b7a64d03e174b22ce72c8
arefmaghdam <arefmaghdam@gmail.com>
2023-01-27 15:11:49 +0100

    fix: resolved "Item not found" problem of material ui grid system

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import styled from "@emotion/styled";
+import { Paper } from "@mui/material";
 import "./App.css";
 import AppRouter from "./AppRouter";
+
+export const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return <AppRouter />;
