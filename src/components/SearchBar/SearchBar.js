ab9d381c66e5de0326a2958e413ed3e7810e56ad
arefmaghdam <arefmaghdam@gmail.com>
2023-02-10 18:12:28 +0100

    feat: completed searchbar component design with optimizing css styles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,22 +1,33 @@
 import { Typeahead } from "react-bootstrap-typeahead";
 import FormGroup from "react-bootstrap/FormGroup";
 import InputGroup from "react-bootstrap/InputGroup";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import style from "./SearchBar.module.css";
+import { BsSearch } from "react-icons/bs";
+import { MdKeyboardVoice } from "react-icons/md";
 
 const SearchBar = () => {
   return (
     <>
-      <FormGroup>
-        <InputGroup>
-          <Typeahead
-          options={["aref" , "saeed"]}
-            id="form-example"
-            labelKey="capital"
-            placeholder="Select a capital..."
-          />
-          <Button>S</Button>
-        </InputGroup>
-      </FormGroup>
+      <div className={`${style.typeAheadContainer}`}>
+        <FormGroup>
+          <InputGroup>
+            <Typeahead
+              options={["react crash course", "frontend crash course"]}
+              id="form-example"
+              labelKey="capital"
+              placeholder="Search"
+              className={`${style.typeAhead}`}
+            />
+            <Button>
+              <BsSearch />
+            </Button>
+          </InputGroup>
+        </FormGroup>
+      </div>
+      <div className={style.mcp}>
+        <button className="btn"><MdKeyboardVoice /></button>
+      </div>
     </>
   );
 };
