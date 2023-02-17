278ac6bb0ffab2aec50f5fb45114f0dcb2eb9917
arefmaghdam <arefmaghdam@gmail.com>
2023-02-17 19:44:22 +0100

    feat: designed options button with css styles and completed signin component

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,10 +1,12 @@
 import style from "./Signin.module.css"
 import { FaRegUserCircle } from "react-icons/fa";
+import { SlOptionsVertical } from "react-icons/sl";
 
 const Signin = () => {
     return (
         <>
             <button className={`btn ${style.signinButton}`}><FaRegUserCircle className={style.userbtn}/><a>Sign in</a></button>
+            <button className={style.optionbtn}><SlOptionsVertical/></button>
         </>
     )
 }
