76b742e8f9019d4ede82e925a30e6cdb97a3ee1d
arefmaghdam <arefmaghdam@gmail.com>
2023-02-19 17:16:44 +0100

    refactor: refactored css styles for whole of project for having a responsive app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import style from "./Layout.module.css";
 import { FaYoutube } from "react-icons/fa";
 import { SlMenu } from "react-icons/sl";
 import SearchBar from "../SearchBar/SearchBar";
+import Signin from "../Signin/Signin";
 
 const Layout = () => {
   return (
@@ -12,12 +13,20 @@ const Layout = () => {
             <a className={style.homeMenu} href="">
               <SlMenu />
             </a>
-            <a className={style.homeIcon} href=""><FaYoutube/></a>
-            <a href="" className={style.youtube}>YouTube</a>
+            <a className={style.homeIcon} href="">
+              <FaYoutube />
+            </a>
+            <a href="" className={style.youtube}>
+              YouTube
+            </a>
           </div>
         </div>
-        <div className={`col-md-8 ${style.searchbarBox}`}><SearchBar/></div>
-        <div className={`col-md-2 ${style.signinBox}`}>signin</div>
+        <div className={`col-md-8 ${style.searchbarBox}`}>
+          <SearchBar />
+        </div>
+        <div className={`col-md-2 ${style.signinBox}`}>
+          <Signin />
+        </div>
       </div>
       <div className={`row ${style.LayoutBody}`}>
         <div className={`col-md-8 ${style.playerBox}`}>video player</div>
