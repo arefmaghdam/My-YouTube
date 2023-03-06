cea95508a62bce3e6e73a171499099fb86f0137e
arefmaghdam <arefmaghdam@gmail.com>
2023-03-06 11:14:27 +0100

    refactor: changed codes for having a responsibility in videoplayer component

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,5 +1,6 @@
 import YouTube from "react-youtube";
 import YouTubeProps from "react-youtube";
+import style from "./VideoPlayer.module.css";
 
 const VideoPlayer = () => {
   const onPlayerReady = (YouTubeProps["onReady"] = (event) => {
@@ -7,12 +8,16 @@ const VideoPlayer = () => {
   });
 
   const opts = (YouTubeProps["opts"] = {
-    height: "485",
-    width: "850",
+    height: "100%",
+    width: "100%",
     // playerVars: "https://developers.google.com/youtube/player_parameters",
   });
 
-  return <YouTube videoId="6QfN3ghaUfE" opts={opts} onReady={onPlayerReady} />;
+  return (
+    <div className={style.videoStyle}>
+      <YouTube videoId="6QfN3ghaUfE" opts={opts} onReady={onPlayerReady} />
+    </div>
+  );
 };
 
 export default VideoPlayer;
