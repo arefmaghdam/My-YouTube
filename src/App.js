98f546abc73f3233030aa984f4f4f2bdd929ecca
arefmaghdam <arefmaghdam@gmail.com>
2023-04-06 19:41:33 +0200

    feat: implemented redux Provider in whole of project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import styled from "@emotion/styled";
 import { Paper } from "@mui/material";
 import "./App.css";
@@ -6,12 +8,16 @@ import AppRouter from "./AppRouter";
 export const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
 
 function App() {
-  return <AppRouter />;
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
 }
 
 export default App;
