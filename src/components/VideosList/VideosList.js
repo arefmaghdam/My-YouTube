de9a50e0aa044367d1836673be2bace302d79696
arefmaghdam <arefmaghdam@gmail.com>
2023-04-14 16:25:55 +0200

    feat: added some videos manually with responsive css styles with height width ratio of 16:9 for video frames

--- src/components/VideosList/VideosList.js
+++ src/components/VideosList/VideosList.js
@@ -1,39 +1,66 @@
-import style from "./VideosList.module.css"
+import YouTube from "react-youtube";
+import style from "./VideosList.module.css";
+import { YouTubeProps } from "react-youtube";
 
 const VideosList = () => {
-    return (
-        <div className="container">
-              <div className="row">
-                <div className="col-md-12">
-                    <div className={style.category}>
-                        <button className={`btn ${style.categoryButtons}`}>All</button>
-                        <button className={`btn ${style.categoryButtons}`}>Live</button>
-                        <button className={`btn ${style.categoryButtons}`}>Music</button>
-                        <button className={`btn ${style.categoryButtons}`}>Chill-out music</button>
-                        <button className={`btn ${style.categoryButtons}`}>JavaScript</button>
-                        <button className={`btn ${style.categoryButtons}`}>Jazz</button>
-                        <button className={`btn ${style.categoryButtons}`}>Gaming</button>
-                        <button className={`btn ${style.categoryButtons}`}>Ed Shiran</button>
-                        <button className={`btn ${style.categoryButtons}`}>Algorithms</button>
-                        <button className={`btn ${style.categoryButtons}`}>Gadgets</button>
-                        <button className={`btn ${style.categoryButtons}`}>Comedy</button>
-                        <button className={`btn ${style.categoryButtons}`}>Pop Music</button>
-                    </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-4">
-                    <div className={style.videos}>videos</div>
-                </div>
-                <div className="col-md-4">
-                <div className={style.videos}>videos</div>
-                </div>
-                <div className="col-md-4">
-                <div className={style.videos}>videos</div>
-                </div>
-              </div>
-            </div>
-    )
-}
+  const opts = {
+    width: "",
+    height: "",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <div className={style.category}>
+            <button className={`btn ${style.categoryButtons}`}>All</button>
+            <button className={`btn ${style.categoryButtons}`}>Live</button>
+            <button className={`btn ${style.categoryButtons}`}>Music</button>
+            <button className={`btn ${style.categoryButtons}`}>
+              Chill-out music
+            </button>
+            <button className={`btn ${style.categoryButtons}`}>
+              JavaScript
+            </button>
+            <button className={`btn ${style.categoryButtons}`}>Jazz</button>
+            <button className={`btn ${style.categoryButtons}`}>Gaming</button>
+            <button className={`btn ${style.categoryButtons}`}>
+              Ed Shiran
+            </button>
+            <button className={`btn ${style.categoryButtons}`}>
+              Algorithms
+            </button>
+            <button className={`btn ${style.categoryButtons}`}>Gadgets</button>
+            <button className={`btn ${style.categoryButtons}`}>Comedy</button>
+            <button className={`btn ${style.categoryButtons}`}>
+              Pop Music
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-4">
+          <div className={style.videos}>
+            <YouTube videoId="R7roiId3--U" opts={opts} />
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className={style.videos}>
+            <YouTube videoId="6QfN3ghaUfE" opts={opts} />
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className={style.videos}>
+            <YouTube videoId="fj9pIe4XMeI" opts={opts} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default VideosList;
