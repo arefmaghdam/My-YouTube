37d0ce760f474559b5683f14470df8ce9ffdbf00
arefmaghdam <arefmaghdam@gmail.com>
2023-05-01 15:10:11 +0200

    feat: sent axios request and recieved datas

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Typeahead } from "react-bootstrap-typeahead";
 import FormGroup from "react-bootstrap/FormGroup";
 import InputGroup from "react-bootstrap/InputGroup";
@@ -5,8 +6,25 @@ import Button from "@mui/material/Button";
 import style from "./SearchBar.module.css";
 import { BsSearch } from "react-icons/bs";
 import { MdKeyboardVoice } from "react-icons/md";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("All");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchTerm}&key=AIzaSyB2N5UT2XMF5BB96S5wEVauTxjFMklOB5s`
+      )
+      .then((response) => {
+        if (response.status !== 200) return;
+        console.log(response.data.items);
+      })
+      .catch((err) => {
+        console.log(err.code);
+      });
+  }, []);
   return (
     <>
       <div className={`${style.typeAheadContainer}`}>
@@ -26,7 +44,9 @@ const SearchBar = () => {
         </FormGroup>
       </div>
       <div className={style.mcp}>
-        <button className="btn"><MdKeyboardVoice /></button>
+        <button className="btn">
+          <MdKeyboardVoice />
+        </button>
       </div>
     </>
   );
